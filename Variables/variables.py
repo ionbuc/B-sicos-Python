3fb9c7d635f5123bea56003c1c14b22e29a8ece0
Enrique Buchán <ionbuc@hotmail.com>
2022-09-20 14:35:16 -0500

    cambios de comillas

--- Variables/variables.py
+++ Variables/variables.py
@@ -1,5 +1,6 @@
-y = 1 + x * 2 # variable con una operación matemática 
-z = (“hola a todos”) # variable de caracteres 
-w = input (“ingresa tu color favorito”) # variable de ingreso de caracteres 
-cantidad = int (input (“Ingresa tu edad”) # variable de ingreso numérico entero 
-peso = float (input (“¿cuánto pesas?”)) # variable de ingreso de tipo decimal 
+x = 73
+y = 1 + x *2 # variable con una operación matemática 
+z = (" a todos") # variable de caracteres 
+w = input ("ingresa tu color favorito ") # variable de ingreso de caracteres 
+cantidad = int (input ("Ingresa tu edad ")) # variable de ingreso numérico entero 
+peso = float (input ("¿cuánto pesas? ")) # variable de ingreso de tipo decimal 
